97a8a8ad73bffd85e7230de7c4aaef373720b9b5
yanhong <yanhong.yan@alibaba-inc.com>
2023-07-05 15:02:54 +0800

    补充缺失字段，新增通知内容体

--- src/main/java/com/alipay/global/api/request/ams/notify/AlipayCaptureResultNotify.java
+++ src/main/java/com/alipay/global/api/request/ams/notify/AlipayCaptureResultNotify.java
@@ -4,11 +4,36 @@ import com.alipay.global.api.model.ams.Amount;
 
 public class AlipayCaptureResultNotify extends AlipayNotify{
 
+    /**
+     * The unique ID that is assigned by the merchant to identify a capture request
+     */
     private String captureRequestId;
 
+    /**
+     * The unique ID that is assigned by Alipay to identify a payment
+     */
     private String paymentId;
 
+    /**
+     * The unique ID assigned by Alipay to identify a capture
+     */
+    private String captureId;
+
+    /**
+     * The capture amount that the merchant requests to receive in the transaction currency
+     */
     private Amount captureAmount;
+
+    /**
+     * The time when Alipay captures the payment
+     */
+    private String captureTime;
+
+    /**
+     * The unique ID assigned by the non-Alipay acquirer for the transaction
+     */
+    private String acquirerReferenceNo;
+
 
     public String getCaptureRequestId() {
         return captureRequestId;
@@ -32,5 +57,29 @@ public class AlipayCaptureResultNotify extends AlipayNotify{
 
     public void setCaptureAmount(Amount captureAmount) {
         this.captureAmount = captureAmount;
+    }
+
+    public String getCaptureId() {
+        return captureId;
+    }
+
+    public void setCaptureId(String captureId) {
+        this.captureId = captureId;
+    }
+
+    public String getCaptureTime() {
+        return captureTime;
+    }
+
+    public void setCaptureTime(String captureTime) {
+        this.captureTime = captureTime;
+    }
+
+    public String getAcquirerReferenceNo() {
+        return acquirerReferenceNo;
+    }
+
+    public void setAcquirerReferenceNo(String acquirerReferenceNo) {
+        this.acquirerReferenceNo = acquirerReferenceNo;
     }
 }
---
+++ src/main/java/com/alipay/global/api/request/ams/notify/AlipayRefundNotify.java
@@ -0,0 +1,102 @@
+package com.alipay.global.api.request.ams.notify;
+
+import com.alipay.global.api.model.ams.Amount;
+import com.alipay.global.api.model.ams.Quote;
+
+/**
+ * @Author yanhong
+ * @version $Id: AlipayRefundNotify.java, v 0.1 2023年07月05日 2:50 PM yanhong Exp $
+ **/
+public class AlipayRefundNotify extends AlipayNotify{
+
+    /**
+     * Indicates the refund result. SUCCESS/FAIL
+     */
+    private String refundStatus;
+
+    /**
+     * The unique ID assigned by a merchant to identify a refund request
+     */
+    private String refundRequestId;
+
+    /**
+     * The unique ID that is assigned by Alipay to identify a refund. A one-to-one correspondence between paymentId and paymentRequestId exists
+     */
+    private String refundId;
+
+    /**
+     * The refund amount that is initiated by the merchant
+     */
+    private Amount refundAmount;
+
+    /**
+     * The date and time when the refund reaches a final state of success or failure
+     */
+    private String refundTime;
+
+    /**
+     * The refund settlement amount, which equals the refund amount multiplied by the value of settlementQuote
+     */
+    private Amount grossSettlementAmount;
+
+    /**
+     * The exchange rate between the settlement currency and transaction currency
+     */
+    private Quote settlementQuote;
+
+    public String getRefundStatus() {
+        return refundStatus;
+    }
+
+    public void setRefundStatus(String refundStatus) {
+        this.refundStatus = refundStatus;
+    }
+
+    public String getRefundRequestId() {
+        return refundRequestId;
+    }
+
+    public void setRefundRequestId(String refundRequestId) {
+        this.refundRequestId = refundRequestId;
+    }
+
+    public String getRefundId() {
+        return refundId;
+    }
+
+    public void setRefundId(String refundId) {
+        this.refundId = refundId;
+    }
+
+    public Amount getRefundAmount() {
+        return refundAmount;
+    }
+
+    public void setRefundAmount(Amount refundAmount) {
+        this.refundAmount = refundAmount;
+    }
+
+    public String getRefundTime() {
+        return refundTime;
+    }
+
+    public void setRefundTime(String refundTime) {
+        this.refundTime = refundTime;
+    }
+
+    public Amount getGrossSettlementAmount() {
+        return grossSettlementAmount;
+    }
+
+    public void setGrossSettlementAmount(Amount grossSettlementAmount) {
+        this.grossSettlementAmount = grossSettlementAmount;
+    }
+
+    public Quote getSettlementQuote() {
+        return settlementQuote;
+    }
+
+    public void setSettlementQuote(Quote settlementQuote) {
+        this.settlementQuote = settlementQuote;
+    }
+}
