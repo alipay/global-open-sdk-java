84108f380136768521d861553f088da023512fc3
国栋 <wangzunjiao.wzj@digital-engine.com>
2025-04-11 14:40:02 +0800

    update 250411

--- src/main/java/com/alipay/global/api/request/ams/notify/AlipayVaultingNotify.java
+++ src/main/java/com/alipay/global/api/request/ams/notify/AlipayVaultingNotify.java
@@ -1,6 +1,7 @@
 package com.alipay.global.api.request.ams.notify;
 
 import com.alipay.global.api.model.ams.AcquirerInfo;
+import com.alipay.global.api.model.ams.CustomizedInfo;
 import com.alipay.global.api.model.ams.VaultingPaymentMethodDetail;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -22,4 +23,6 @@ public class AlipayVaultingNotify extends AlipayNotify {
     private String vaultingCreateTime;
 
     private AcquirerInfo acquirerInfo;
+
+    private CustomizedInfo customizedInfo;
 }
