725bacd0aa51e58de0b7585f570b74ce0e1ff0ba
王尊姣 <wangzunjiao.wzj@digital-engine.com>
2026-01-14 10:05:09 +0800

    update SubscriptionNotificationType

--- src/main/java/com/alipay/global/api/model/ams/SubscriptionNotificationType.java
+++ src/main/java/com/alipay/global/api/model/ams/SubscriptionNotificationType.java
@@ -1,8 +1,9 @@
 package com.alipay.global.api.model.ams;
 
 public enum SubscriptionNotificationType {
-  CREATE,
-  CHANGE,
-  CANCEL,
-  TERMINATE;
+    CREATE,
+    CHANGE,
+    CANCEL,
+    UPDATE,
+    TERMINATE;
 }
