fe9c50e1a0a26762d5928e04ea07b66917c621ef
毅城 <zhangqizeng.zqz@digital-engine.com>
2026-07-23 11:10:07 +0800

    feat: add updateRequestId and authExpiryTime to AlipayPayResultNotify for UPDATE_AMOUNT_RESULT notify

--- src/main/java/com/alipay/global/api/request/ams/notify/AlipayPayResultNotify.java
+++ src/main/java/com/alipay/global/api/request/ams/notify/AlipayPayResultNotify.java
@@ -65,4 +65,13 @@ public class AlipayPayResultNotify extends AlipayNotify {
 
   private String subscriptionOrderId;
   private RetryInfo retryInfo;
+
+  /** The updateAmount request ID. Returned when notifyType=UPDATE_AMOUNT_RESULT. */
+  private String updateRequestId;
+
+  /**
+   * The absolute expiry time of this pre-authorization. Returned when auth is successful.
+   * Multiple updateAmount calls do not reset this value. ISO 8601 format string.
+   */
+  private String authExpiryTime;
 }
